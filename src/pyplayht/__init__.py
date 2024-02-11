7c4089be4726a3af904090c5f26eb1e694604d3d
Sean Summers <seansummers@gmail.com>
2024-02-10 23:01:18 -0500

    initial working version

---
+++ src/pyplayht/__init__.py
@@ -0,0 +1,4 @@
+from .auth import *
+from .tts import *
+
+__all__ = ["Credentials", "Session", "HyperParameters", "JobParameters", "GrpcTts"]
---
+++ src/pyplayht/logging_config.py
@@ -0,0 +1,12 @@
+from sys import stderr
+
+from eliot import log_call, to_file
+
+to_file(stderr)
+import logging
+
+from eliot.stdlib import EliotHandler
+
+logging.getLogger().addHandler(EliotHandler())
+
+__all__ = ["logging", "log_call"]
